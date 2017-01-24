2427ce3944d9d43db8cacc07b314fda5f199bb95
Omid Khataee <maverickxm2@live.ca>
2017-01-24 14:02:51 -0500

    Routed all ejs pages with very barebones template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index' });
 });
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+/* GET products page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Products' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contacts' });
+});
+
 
 module.exports = router;
